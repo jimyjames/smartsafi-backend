cb4a139be800399ab162e6f85f364853d71755de
jimyjames <sjmuriithi@gmail.com>
2025-12-17 15:24:44 +0300

    still fixing my sh**t F*****y

---
+++ smart-api/payments/__init__.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter
+import requests
+from requests.auth import HTTPBasicAuth
+from datetime import datetime
+import base64
+import stripe
+import os
+
+def lipa_na_mpesa_online(phone: str ="254759234753", amount: float= 1.0):
+    # Step 1: Get access token
+    consumer_key = os.getenv("Mpesa_Consumer_Key")
+    consumer_secret = os.getenv("Mpesa_Consumer_Secret")
+    auth_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+    
+    auth_response = requests.get(auth_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+    access_token = auth_response.json()["access_token"]
+
+    # Step 2: Prepare STK Push payload
+    shortcode = "174379"
+    # phone="254790329962"
+    passkey = os.getenv("Mpesa_Passkey")
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    
+    password = base64.b64encode((shortcode + passkey + timestamp).encode()).decode()
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json"
+    }
+
+    payload = {
+        "BusinessShortCode": shortcode,
+        "Password": password,
+        "Timestamp": timestamp,
+        "TransactionType": "CustomerPayBillOnline",
+        "Amount": amount,
+        # "PartyA": "254759234753",  # customer's phone number
+        "PartyA": phone,
+        "PartyB": shortcode,
+        # "PhoneNumber": "254759234753",  # same as PartyA
+        "PhoneNumber": phone,
+        "CallBackURL": "https://yourdomain.com/callback",
+        "AccountReference": "Test123",
+        "TransactionDesc": "Payment for goods"
+    }
+    print("as received" , phone, amount)
+    response = requests.post(
+        "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest",
+        headers=headers,
+        json=payload
+    )
+
+    return response.json()
+
+
+def stripe_payment_test(amount: float= 100.0, currency: str ="usd", source: str ="tok_visa"):
+    stripe.api_key = os.getenv("stripe_api_key")
+
+    try:
+        charge = stripe.Charge.create(
+            amount=int(amount * 100),  # Amount in cents
+            currency=currency,
+            source=source,
+            description="Test Charge"
+        )
+        return charge
+    except stripe.error.StripeError as e:
+        return {"error": str(e)}
+
+
+router = APIRouter(prefix="/payments", tags=["payments"])
